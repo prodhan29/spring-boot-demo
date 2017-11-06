e530242e315f802eae75c5cf8cf20d0d3908470f
nahid hasan <02nahid02@gmail.com>
2017-11-06 18:10:59 +0600

    use repository added

--- src/main/java/com/moto/spring_demo/controller/HomeController.java
+++ src/main/java/com/moto/spring_demo/controller/HomeController.java
@@ -1,13 +1,20 @@
 package com.moto.spring_demo.controller;
 
+import com.moto.spring_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("")
     public String home(){
+
         return "hello  world";
     }
 }
---
+++ src/main/java/com/moto/spring_demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.moto.spring_demo.repository;
+
+import com.moto.spring_demo.domain.User;
+import org.hibernate.boot.model.source.spi.JpaCallbackSource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
